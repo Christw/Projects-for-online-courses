25c65e42e8cd94c657caa1e473ba4d70549a766f
Christw <67684645+Christw@users.noreply.github.com>
2020-09-26 15:15:49 +0800

    Add files via upload

---
+++ Capstone Project.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Capstone Project
+
+# In[4]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[5]:
+
+
+print('Hello Capstone Project Course!')
+
